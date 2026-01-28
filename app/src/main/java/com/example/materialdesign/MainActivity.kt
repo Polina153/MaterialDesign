f3504dbd3ea42ab7d53e23db021f8b5ff09dbf0f
Linka <Polina153@mail.ru>
2026-01-28 17:33:22 +0400

    SplashScreen + background and logo

---
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.materialdesign
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        setInsets()
+
+        setSplashScreenLoadingParameters()
+    }
+
+    private fun setSplashScreenLoadingParameters() {
+        var isHideSplashScreen = false
+
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
+    }
+
+    private fun setInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
